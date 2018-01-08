4295654dc6198da54dd0ec3c9eb17b82501ec73f
Emilio Molinari <xates97@gmail.com>
2018-01-08 21:48:55 +0100

    Fixed #1. Now when a message matches a pattern and it's forwarded, it's also marked as read

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 import configparser
 import re
 
 from telethon import *
-from telethon.tl.functions.channels import *
-from telethon.tl.functions.messages import *
+from telethon.tl.functions.channels import (JoinChannelRequest,
+                                            ReadHistoryRequest)
+from telethon.tl.functions.messages import ForwardMessagesRequest
 from telethon.tl.types import *
 
 config = configparser.ConfigParser()
@@ -18,14 +21,13 @@ message_filters = [s.strip()
                    for s in settings.get('message_filters').split(',')]
 forward_to = settings.get('forward_to')
 
-client = TelegramClient(session_name, api_id, api_hash, update_workers=1,
-                        spawn_read_thread=False)
+client = TelegramClient(session_name, api_id, api_hash,
+                        update_workers=1, spawn_read_thread=False)
 client.connect()
 
 if client.is_user_authorized():
     me = client.get_me()
 else:
-    # TODO: manage empty and invalid inputs
     user_phone = input('Enter your phone number in international format: ')
     client.send_code_request(user_phone)
     me = client.sign_in(user_phone,
@@ -37,26 +39,25 @@ for channel in channel_names:
 
 def callback(update):
     """Callback method for received Updates"""
-    print('I received', update)  # TODO: delete this line
+    print('I received', update)
     if (isinstance(update, UpdateNewChannelMessage)
             or isinstance(update, UpdateEditChannelMessage)):
         msg = update.message
         channel = client.get_entity(msg.to_id)
-        for name in channel_names:
-            if channel.username == name:
-                for pattern in message_filters:
-                    if (re.match(pattern, msg.message, re.I | re.S)
-                            is not None):
-                        client(ForwardMessagesRequest(
-                            from_peer=channel,
-                            id=[msg.id],
-                            to_peer=client.get_input_entity(forward_to)
-                        ))
-                        client(ReadHistoryRequest(
-                            channel=channel,
-                            max_id=msg.id
-                        ))
-                        return
+        if channel.username.lower() in channel_names:
+            for pattern in message_filters:
+                if (re.match(pattern, msg.message, re.I | re.S)
+                        is not None):
+                    client(ForwardMessagesRequest(
+                        from_peer=channel,
+                        id=[msg.id],
+                        to_peer=client.get_input_entity(forward_to)
+                    ))
+                    client(ReadHistoryRequest(
+                        channel=channel,
+                        max_id=msg.id
+                    ))
+                    break
 
 
 client.add_update_handler(callback)
